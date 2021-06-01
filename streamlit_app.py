c241f4603d650e345552e884cf0f02b6da0e6cff
Rawnak Mahjabib <72884366+RawnakMahjabib@users.noreply.github.com>
2021-06-01 18:44:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,11 @@ if st.button("Find Flights", key='55'):
     st.markdown("## FLIGHT DASHBOARD")
     st.text(f"Current Flights from {departure_iata} to {arrival_iata}")
     #load_dotenv("api_key.env")
-    aviationstack_api = st.secrets["AVIATIONSTACK_API_KEY"]
+    os.environ["AVIATIONSTACK_API_KEY"] == st.secrets["AVIATIONSTACK_API_KEY"]
+    aviationstack_api = os.environ["AVIATIONSTACK_API_KEY"]
     aviationstack_url = 'http://api.aviationstack.com/v1/flights'
     params = {
-  'access_key': st.secrets["AVIATIONSTACK_API_KEY"],
+  'access_key': aviationstack_api,
   'dep_iata': departure_iata,
   'arr_iata': arrival_iata,
 }
